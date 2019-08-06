ebc3ad8817f947d885227b1cd5945ced0c4d4e33
anggabanny <anggabanny@gmail.com>
2019-08-06 20:01:12 +0700

    🔜

---
+++ exercise-4.js
@@ -0,0 +1,18 @@
+function cariModus(arr) {
+    // you can only write your code here!
+    // tampung frekuensi, frekuensi terbesar, modus assign 0
+    // looping 2 kali, bandingkan i dgn j serta j tdk sama i 
+    // masukan ke frekuensi lalu bandingkan dengan frek terbesar
+    // tampung modus dengan i 
+    // jika frekuensi nol return -1 
+    // cek angka sama semua, tampung variable
+    //looping k lalu masukan hasilnya ke variabel tadi 
+    // jika jumlah angka sama return -1
+}
+
+// TEST CASES
+console.log(cariModus([10, 4, 5, 2, 4])); // 4
+console.log(cariModus([5, 10, 10, 6, 5])); // 5
+console.log(cariModus([10, 3, 1, 2, 5])); // -1
+console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
+console.log(cariModus([7, 7, 7, 7, 7])); // -1
